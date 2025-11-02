873f979acefeb76fa9e8e760f50bf7336cc7d34f
AyzakLB <79733183+AyzakLB@users.noreply.github.com>
2025-11-02 05:31:04 +0100

    set up of portainer and static website done

--- srcs/requirements/wordpress/tools/wordpress_init.sh
+++ srcs/requirements/wordpress/tools/wordpress_init.sh
@@ -32,7 +32,7 @@ wp user create \
     --allow-root
 
 wp --allow-root plugin install redis-cache
-until reids-cli -h redis ping ; do
+until redis-cli -h redis ping ; do
     sleep 1
 done
 wp --allow-root config set WP_REDIS_HOST "redis"
